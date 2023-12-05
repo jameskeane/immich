a753832c0496d63b00627bf1f7848d838c461865
shalong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2023-12-06 03:15:45 +0530

    refactor: migrate album sort option to provider

--- mobile/lib/modules/settings/services/app_settings.service.dart
+++ mobile/lib/modules/settings/services/app_settings.service.dart
@@ -51,6 +51,11 @@ enum AppSettingsEnum<T> {
   mapIncludeArchived<bool>(StoreKey.mapIncludeArchived, null, false),
   mapRelativeDate<int>(StoreKey.mapRelativeDate, null, 0),
   allowSelfSignedSSLCert<bool>(StoreKey.selfSignedCert, null, false),
+  selectedAlbumSortReverse<bool>(
+    StoreKey.selectedAlbumSortReverse,
+    null,
+    false,
+  ),
   ;
 
   const AppSettingsEnum(this.storeKey, this.hiveKey, this.defaultValue);
